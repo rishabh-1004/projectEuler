2a652b8947a6c2b9a821b2be3d91606d1304b359
Rishabh <rishabh.sharma@tryscribe.com>
2019-04-12 18:44:55 +0530

    ProjectEuler1

---
+++ euler7.py
@@ -0,0 +1,29 @@
+###--Euler7--##
+import time 
+
+def isPrime(primes,number):
+    for p in primes.keys():
+      if (number%p==0):
+        return False
+    primes[number]=True
+    return True
+  
+
+def main():
+    counter=0
+    primes=dict()
+    for i in range(2,10000000000):
+      if (isPrime(primes,i)):
+        counter=counter+1
+        if (counter==10001):
+          print(i)
+          break
+    
+  
+if __name__=="__main__":  
+  start=time.perf_counter()
+  main()
+  print(f"Solution took: {time.perf_counter()-start}") 
+
+#104743
+#Solution took: 3.0644659470001443
