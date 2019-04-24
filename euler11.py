91ba55d4b753a9a2bbe317ba103db6e5cbadcadd
rishabh-1004 <rishabhrajshrm00@gmail.com>
2019-04-24 07:52:47 +0530

    More Solutions

---
+++ euler11.py
@@ -0,0 +1,34 @@
+with open('euler11.txt','r') as files:
+	array=[]
+	for each in files:
+		array.append(each)
+		#print (each)
+#print(array)
+
+newArray=[]
+for i in array:
+	j=i.split(' ')
+	k=[int(n) for n in j]
+	newArray.append(k)
+#print(newArray)
+#print(newArray[0])
+
+#print((newArray[0][0])*(newArray[1][1]))
+
+maxprod=0
+for i in range(16):
+	for j in range(16):	
+		prod_row=(newArray[i][j])*(newArray[i+1][j])*(newArray[i+2][j])*(newArray[i+3][j])
+		if prod_row>maxprod:
+			maxprod=prod_row
+		prod_col=newArray[i][j]*newArray[i][j+1]*newArray[i][j+2]*newArray[i][j+3]
+		if prod_col>maxprod:
+			maxprod=prod_col
+		prod_diag=newArray[i][j]*newArray[i+2][j+2]*newArray[i+2][j+2]*newArray[i+3][j+3]
+		if prod_diag>maxprod:
+			maxprod=prod_diag
+		prod = newArray[19-i][j]*newArray[18-i][j+1]*newArray[17-i][j+2]*newArray[16-i][j+3]
+		if prod>maxprod:
+			maxprod=prod
+
+print (maxprod)
---
+++ euler12.py
@@ -0,0 +1,20 @@
+import time 
+from sympy import factorint
+
+start= time.perf_counter()
+
+for i in range(10000,10_000_000):
+	list_sum=sum([num for num in range(i+1)])
+	counter=1
+	fact_dict=factorint(list_sum)
+	#print(fact_dict)
+
+	for j in fact_dict.values():
+		counter*=(j+1)
+
+	if counter>500:
+		print(list_sum)
+		break
+
+
+print(f'time taken : {time.perf_counter()-start}' )
